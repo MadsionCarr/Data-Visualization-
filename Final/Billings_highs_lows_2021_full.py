cc153cdf069d5e3bd6839ba9dad2940439ed6c13
MadsionCarr <90936149+MadsionCarr@users.noreply.github.com>
2021-12-15 21:25:09 -0700

    Add files via upload

---
+++ Final/Billings_highs_lows_2021_full.py
@@ -0,0 +1,38 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+filename = 'data/Billings_air_temp_2021.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+    #plot high temperature
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+    ax.plot(dates, highs, c='red', alpha=.5)
+    ax.plot(dates, lows, c='blue', alpha=.5)
+    ax.fill_between(dates, highs, lows, facecolor='blue', alpha=.1)
+
+    #format plot
+    title = "Daily high and low temperatures,  2012 Billings, MT"
+    ax.set_title(title, fontsize=20)
+    ax.set_xlabel('', fontsize=16)
+    fig.autofmt_xdate()
+    ax.set_ylabel('Temperature (F)', fontsize=16)
+    ax.tick_params(axis='both', which='major', labelsize=16)
+
+    plt.show()
